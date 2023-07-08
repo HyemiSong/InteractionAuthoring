02952324260111f67913fb15ae84bb2b0c872824
hyemi <hsong@umd.edu>
2023-07-07 23:27:51 -0400

    connect server to mongoose

--- server/index.js
+++ server/index.js
@@ -1,6 +1,21 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
-import cores from 'cors';
+import cors from 'cors';
 
-const express = require('express')
+const app = express();
+
+app.use(bodyParser.json({ limit: '30mb', extended: true }))
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
+app.use(cors());
+
+const CONNECTION_URL = 'mongodb+srv://hsong02:Chacolina11017@cluster0.j9w8yhn.mongodb.net/?retryWrites=true&w=majority'
+const PORT = process.env.PORT|| 5000;
+
+mongoose.connect(CONNECTION_URL).then(()=>{console.log(`Server Running on Port: http://localhost:${PORT}`)})
+
+//deprecated
+// mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+//   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
+//   .catch((error) => console.log(`${error} did not connect`));
+// mongoose.set('useFindAndModify', false);
