bd1250f59779d60bff53d993f233ff201910ef9e
hyemi <hsong@umd.edu>
2023-07-08 00:18:26 -0400

    server is connected

--- server/index.js
+++ server/index.js
@@ -3,16 +3,19 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 
+import postRoutes from './routes/posts.js';
+
 const app = express();
 
-app.use(bodyParser.json({ limit: '30mb', extended: true }))
-app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
+app.use(bodyParser.json({ limit: '30mb', extended: true }));
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
+app.use('/posts', postRoutes);
 
 const CONNECTION_URL = 'mongodb+srv://hsong02:Chacolina11017@cluster0.j9w8yhn.mongodb.net/?retryWrites=true&w=majority'
-const PORT = process.env.PORT|| 5000;
+const PORT = process.env.PORT|| 4000;
 
-mongoose.connect(CONNECTION_URL).then(()=>{console.log(`Server Running on Port: http://localhost:${PORT}`)})
+mongoose.connect(CONNECTION_URL).then(()=> app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
 
 //deprecated
 // mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
