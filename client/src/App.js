3259a8451766fc6a2b2f63a10b805a3d1269c068
hyemi <hsong@umd.edu>
2023-07-14 22:44:10 -0400

    card UI, grid, index.css

--- client/src/App.js
+++ client/src/App.js
@@ -19,7 +19,7 @@ const App = () => {
     return(
     <Container maxWidth="lg">
       <AppBar className={classes.appBar} position="static" color="inherit">
-        <Typography className={classes.heading} variant="h2" align="center" hight="60">Memories</Typography>
+        <Typography className={classes.heading} variant="h5" align="center" hight="60">INTENTS, TECHNIQUES, AND COMPONENTS: A UNIFIED ACCOUNT</Typography>
         <img src={memories} alt="icon" height="60" />
       </AppBar>
       <Grow in>
