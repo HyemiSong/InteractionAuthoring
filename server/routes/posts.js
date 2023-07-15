379418a9b6ad2f9b21481fa534b06cf2f0e19222
hyemi <hsong@umd.edu>
2023-07-14 22:42:32 -0400

    card UI, grid, index.css

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -4,7 +4,7 @@ import { getPosts, createPost } from '../controllers/posts.js'
 
 const router = express.Router();
 
-// http://localhost:5000/posts
+// http://localhost:4000/posts
 router.get('/', getPosts) 
 router.post('/', createPost) 
 
